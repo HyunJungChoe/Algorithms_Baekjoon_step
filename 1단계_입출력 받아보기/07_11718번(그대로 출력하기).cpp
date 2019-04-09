0830e2339540b1a00c8d8c55d8c7f1e986a2aed9
HyunJungChoe <46165574+HyunJungChoe@users.noreply.github.com>
2019-04-09 22:26:20 +0900

    Add files via upload

---
+++ 1단계_입출력 받아보기/07_11718번(그대로 출력하기).cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	char c;
+	while((c = getchar()) != EOF)
+	{
+		putchar(c);
+	}
+}
+/* 
+EOF = End of File
+getchar() : 문자 입력 
+putchar() : 문자 출력
+즉, getchar()함수에서 차례로 받아 바로 putchar함수로 출력해준다. 
+printf 와 scanf를 사용하면 시간 초과가 된다.  
+*/ 
